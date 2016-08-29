d174b0aabb173f63650e5e46f4137f96a4ed0668
Alexey Ermakov <aermakov@itdhq.com>
2016-08-29 22:12:58 +0300

    Run basic integration tests for whole bundle

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -2,19 +2,28 @@
 
 set -e
 
+function run_server()
+{
+    ./mvnw -f $1/pom.xml -P amp-to-war integration-test -Dmaven.tomcat.port=$2 &
+}
+
 function run_test()
 {
-    ./${5}mvn.sh -f $4$1/pom.xml -P amp-to-war integration-test -Dmaven.tomcat.port=$2 &
-    while ! curl http://127.0.0.1:$2/$3 &> /dev/null; do
+    while ! curl http://127.0.0.1:$1/$2 &> /dev/null; do
         sleep 1;
     done
-    test $( curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:$2/$3 ) -lt 400;
+    test $( curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:$1/$2 ) -lt 400;
 }
 
 if [[ "$1" = "repo" ]]; then
-    run_test repo 8080 alfresco $2 $3;
+    run_server repo 8080
+    run_test 8080 alfresco
 elif [[ "$1" = "share" ]]; then
-    run_test share 8081 share $2 $3;
+    run_server share 8081
+    run_test 8081 share
+elif [[ "$1" = "docker" ]]; then
+    run_test 8080 alfresco
+    run_test 8080 share
 else
     false;
 fi
